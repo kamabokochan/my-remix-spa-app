ae8242cdbbd07e5c471c1b1588191f929b18a571
yamam00s <s.yamamo0327@gmail.com>
2024-02-12 17:44:09 +0900

    hono: API DBの中身を返却する

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -1,10 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client'
 
 const app = new Hono()
+const prisma = new PrismaClient()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+  const todos = await prisma.todos.findMany()
+
+  return c.json(todos)
 })
 
 const port = 3000
