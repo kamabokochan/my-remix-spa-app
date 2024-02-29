ee63f2adde96da54c2ada1f775b51ed49ab4f994
yamam00s <s.yamamo0327@gmail.com>
2024-02-29 23:43:48 +0900

    remix: fetchと表示

--- remix/app/routes/_index.tsx
+++ remix/app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from '@remix-run/react'
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,26 +8,28 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+type Todo = {
+  id: number;
+  title: string
+  status: 'pending' | 'completed'
+}
+
+export async function clientLoader(): Promise<Todo[]> {
+  const response = await fetch('http://localhost:3000/')
+  const data = await response.json()
+  return data
+}
+
 export default function Index() {
+  const dataList = useLoaderData<typeof clientLoader>()
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/future/spa-mode"
-            rel="noreferrer"
-          >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      {dataList.map(data => (
+        <div key={data.id}>
+          <p>{data.title}</p>
+          <p>{data.status}</p>
+        </div>
+      ))}
     </div>
   );
 }
