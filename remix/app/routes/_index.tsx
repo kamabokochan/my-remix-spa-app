9b37432e742a706231f484528a2cfcaeeb2e6c6c
kamabokochan <h.sasaki.worksfront@gmail.com>
2024-03-05 23:03:19 +0900

    remix: 新規TODO追加のaction実装

--- remix/app/routes/_index.tsx
+++ remix/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useLoaderData } from '@remix-run/react'
+import { useLoaderData, Form, ClientActionFunctionArgs, redirect } from '@remix-run/react'
 
 export const meta: MetaFunction = () => {
   return [
@@ -20,14 +20,32 @@ export async function clientLoader(): Promise<Todo[]> {
   return data
 }
 
+export async function clientAction({ request }: ClientActionFunctionArgs) {
+  const body = await request.formData();
+  const response = await fetch('http://localhost:3000/', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({title: body.get('title')})
+  })
+  console.log(response)
+  return redirect("/");
+};
+
 export default function Index() {
   const dataList = useLoaderData<typeof clientLoader>()
+  console.log("🥲")
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <Form method="post">
+        <input type="text" name="title" />
+        <button type="submit">追加</button>
+      </Form>
       {dataList.map(data => (
         <div key={data.id}>
-          <p>{data.title}</p>
-          <p>{data.status}</p>
+          <p>title: {data.title}</p>
+          <p>status: {data.status}</p>
         </div>
       ))}
     </div>
