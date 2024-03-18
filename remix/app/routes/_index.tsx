7ecdc5c6160f0fcbea1ae7d92cd4c30b4c5213d6
yamam00s <s.yamamo0327@gmail.com>
2024-03-18 23:40:35 +0900

    remix: データの更新とstatusの表示

--- remix/app/routes/_index.tsx
+++ remix/app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useLoaderData, Form, ClientActionFunctionArgs, redirect } from '@remix-run/react'
+import { useLoaderData, Form, ClientActionFunctionArgs, redirect, useFetcher } from '@remix-run/react'
+import { FormEvent } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -21,21 +22,39 @@ export async function clientLoader(): Promise<Todo[]> {
 }
 
 export async function clientAction({ request }: ClientActionFunctionArgs) {
-  const body = await request.formData();
-  const response = await fetch('http://localhost:3000/', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({title: body.get('title')})
-  })
-  console.log(response)
+  const baseUrl = 'http://localhost:3000/'
+  const formData = await request.formData()
+  if (request.method === 'PUT') {
+    const id = await formData.get('id');
+    await fetch(`${baseUrl}${id}`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  } else {
+    await fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({title: formData.get('title')})
+    })
+  }
   return redirect("/");
-};
+}
 
 export default function Index() {
   const dataList = useLoaderData<typeof clientLoader>()
-  console.log("🥲")
+  const fetcher = useFetcher()
+
+  const updateStatus = (event: FormEvent<HTMLFormElement>, id: number) => {
+    fetcher.submit(
+      { id },
+      { method: 'PUT' }
+    )
+  }
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <Form method="post">
@@ -44,8 +63,11 @@ export default function Index() {
       </Form>
       {dataList.map(data => (
         <div key={data.id}>
-          <p>title: {data.title}</p>
-          <p>status: {data.status}</p>
+          {<fetcher.Form  method="post" onChange={(event) => updateStatus(event, data.id)}>
+              <input type="checkbox" name="status" id="" checked={data.status === "completed"} />
+            </fetcher.Form>}
+            <p>title: {data.title}</p>
+            <p>status: {data.status}</p>
         </div>
       ))}
     </div>
